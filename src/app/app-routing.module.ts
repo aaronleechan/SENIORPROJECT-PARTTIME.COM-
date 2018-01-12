c44c4e95c14b6e85ce919a07895f55e28cd18230
aaronleechan <aaron171091@gmail.com>
2018-01-11 16:35:52 -0800

    add user

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ import { UsersComponent } from './users/users.component';
 const routes: Routes = [
   { path: 'home', component: HomeComponent},
   { path: 'user', component: UserComponent},
-  { path: 'user-list', component: UserListComponent},
+  { path: 'userlist', component: UserListComponent},
   { path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: 'users', component: UsersComponent}
 
--- src/app/users/user-list/user-list.component.ts
+++ src/app/users/user-list/user-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService} from "../shared/user.service";
+import { User } from "../shared/user.model";
 
 @Component({
   selector: 'app-user-list',
@@ -8,10 +9,19 @@ import { UserService} from "../shared/user.service";
 
 })
 export class UserListComponent implements OnInit {
-
+  userList: User[];
   constructor(private userservice: UserService) { }
 
   ngOnInit() {
+    var x = this.userservice.getData();
+    x.snapshotChanges().subscribe(item => {
+      this.userList = [];
+      item.forEach(element => {
+        var y = element.payload.toJSON();
+        y["$key"] = element.key;
+        this.userList.push(y as User);
+      });
+    });
   }
 
 }
